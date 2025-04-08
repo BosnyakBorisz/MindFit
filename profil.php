b1362d1dd09860b99295b1cf03eb8b5edc092aba
HerczegAndras <herczegandrasgabor@turr.hu>
2025-04-08 09:38:54 +0200

    udvozlunk ! jel

--- profil.php
+++ profil.php
@@ -67,7 +67,7 @@
 
   <main>
     
-    <h1 class="ms-5 mb-5">Üdvözlünk <?php echo "" . htmlspecialchars($_SESSION["username"]) ?></h1>
+    <h1 class="ms-5 mb-5">Üdvözlünk <?php echo "" . htmlspecialchars($_SESSION["username"]) ?>!</h1>
     <p class="text-center" id="napiMoti">""</p>
 
     <section id="calorie">
